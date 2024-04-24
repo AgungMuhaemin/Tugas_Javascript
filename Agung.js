1440d47f92ac4e7b311e5f689e376932d791ae0e
Agung Muhaemin <161552740+AgungMuhaemin@users.noreply.github.com>
2024-04-24 15:55:01 +0700

    Add files via upload

---
+++ Agung.js
@@ -0,0 +1,22 @@
+var nama = "Agung"
+var usia = 21
+var kelas = "PTI"
+var nilai = 80
+console.log(`
+    nama : ${nama}\n
+    usia : ${usia}\n
+    kelas : ${kelas}\n
+    nilai : ${nilai}\n
+`)
+var angka1 = 10
+var angka2 = 20
+console.log(`
+penjumlahan : ${angka1 + angka2}\n
+pengurangan : ${angka1 - angka2}\n
+perkalian : ${angka1 * angka2}\n
+pembagian : ${angka1 / angka2}\n
+`)
+var harga = 50
+var diskon = 0.2
+var totalharga = harga-(harga*diskon)
+console.log(`Total harga adalan: Rp ${totalharga}`)
